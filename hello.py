66a998c2a28cc0cd32762c09663bbf0a2e34d09d
e <princerautit21@gmail.com>
2025-10-30 12:23:16 +0545

    adding inputs

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
+#name= 'prince raut'
+age=input('enter your age:')
+name=input('enter your collage name')
+print(f'Hi your name is: {name}  and your age is {age} ')
+
 print("Hello World")
 print("My Name is Prince Raut")
 #adding comment 
